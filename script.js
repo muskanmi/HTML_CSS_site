9a68a74a26cea11dae6058062139f422ca5005ab
Muskan Mittal <muskan.mittal@w3villa.com>
2023-01-10 17:04:07 +0530

    code attached of template

---
+++ script.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".navbar-style");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
